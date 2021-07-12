74f222f8cc9f7e4edd5b423cc79c64b0bad13b39
Lester Moore Lambac <lester@sokolabs.co>
2021-07-12 23:52:11 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#161E2E',
+        secondary: '#3F75FF',
+        increase: '#32C34A',
+        yellow: '#ECC04B',
+        cyan: '#2EBDC2',
+        purple: '#5242A2',
+      },
+      fontFamily: {
+        'sans': ['Montserrat']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
