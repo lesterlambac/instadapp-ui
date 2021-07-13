79aefb6e6cea0aec3d2870683442d4438efadb4a
Lester Moore Lambac <lester@sokolabs.co>
2021-07-13 13:12:51 +0800

    Assets update

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#161E2E',
-        secondary: '#3F75FF',
-        increase: '#32C34A',
-        yellow: '#ECC04B',
-        cyan: '#2EBDC2',
+        'primary': '#161E2E',
+        'secondary': '#3F75FF',
+        'increase': '#32C34A',
+        'yellow': '#ECC04B',
+        'yellow-100': 'rgba(236, 192, 75, 0.1)',
+        'cyan': '#2EBDC2',
         purple: '#5242A2',
       },
       fontFamily: {
